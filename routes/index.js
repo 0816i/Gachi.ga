c1df18c6fdd2fed5a5936dac6e083f1906a2370d
0816i <0816i@naver.com>
2020-06-28 13:36:43 +0900

    프론트 약간
    에바

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/list', (req, res, next) => {
+  res.render('list', { title: '게시판' })
+});
+
 module.exports = router;
